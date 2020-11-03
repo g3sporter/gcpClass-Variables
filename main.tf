27bdef75bfacddcf7a18e9a1b7c0b06af30ba92f
g3sporter <52547590+g3sporter@users.noreply.github.com>
2020-11-03 14:49:34 -0600

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,24 @@
+resource "google_compute_instance" "default" {
+  count        = length(var.name_count)
+  name         = "list-${count.index + 1}"
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = var.image
+    }
+  }
+  network_interface {
+    network = "default"
+  }
+
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+}
+output "GCP_Instance_Type" { value = "${google_compute_instance.default.*.machine_type}" }
+output "GCP_Instance_Name" { value = "${google_compute_instance.default.*.name}" }
+output "GCP_Instance_Zone" { value = "${google_compute_instance.default.*.zone}" }
+#make the output comma seperated for use as a variable
+output "instance_id" { value = "${join(",", google_compute_instance.default.*.name)}" }
